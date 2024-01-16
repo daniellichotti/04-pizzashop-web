94cb7ac218de295e232a1d9ad487003558f7138a
daniellichotti <danielclichotti@gmail.com>
2024-01-16 18:01:34 -0300

    Paginas e componentes(UI) - Layout do app com cabecalho

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/header'
+
 export function AppLayout() {
   return (
-    <div>
-      <h1>cabecalho</h1>
-      <div>
+    <div className="min-h flex  flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
